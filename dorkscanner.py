fe9d36e2ae03903b54a016d2862cc7725adf1857
kumandra <mutasimplan@protonmail.com>
2021-10-09 13:56:39 -0700

    Dork Scanner

--- dorkscanner.py
+++ dorkscanner.py
@@ -239,8 +239,8 @@ print("""{}
   ____  _ _       _     _     _     _   _             _              [{}+{}] SiteList Hunter v1
  / ___|(_) |_ ___| |   (_)___| |_  | | | |_   _ _ __ | |_ ___ _ __   [{}+{}] Dork Maker and Scanner
  \___ \| | __/ _ \ |   | / __| __| | |_| | | | | '_ \| __/ _ \ '__|  [{}+{}] Get many sites in minutes !
-  ___) | | ||  __/ |___| \__ \ |_  |  _  | |_| | | | | ||  __/ |     [{}+{}] Coded by NinjaCR3
- |____/|_|\__\___|_____|_|___/\__| |_| |_|\__,_|_| |_|\__\___|_|     [{}+{}] Contact : facebook.com/ninjacr3
+  ___) | | ||  __/ |___| \__ \ |_  |  _  | |_| | | | | ||  __/ |     [{}+{}] Coded by FulPlan
+ |____/|_|\__\___|_____|_|___/\__| |_| |_|\__,_|_| |_|\__\___|_|     
 
  Dork Scanner
 """.format(settings.y,settings.r,settings.y,settings.r,settings.y,settings.r,settings.y,settings.r,settings.y,settings.r,settings.y))
